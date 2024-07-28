8260dfeadd0217c2da4904c32c121f9137f3b402
Ashraful Haque Chowdhury <ashraful.rahi@gmail.com>
2024-07-28 13:35:53 +0600

    feat: added main screen feature with bottom nav

--- lib/presentation/features/splash/splash_screen.dart
+++ lib/presentation/features/splash/splash_screen.dart
@@ -1,9 +1,33 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_template/presentation/routes/router.gr.dart';
 
 @RoutePage()
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(const Duration(seconds: 3), () {
+      context.router.replace(const MainRoute());
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,15 +1,29 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter_template/presentation/features/splash/splash_screen.dart';
-
-part 'router.gr.dart';
+import 'package:flutter_template/presentation/routes/router.gr.dart';
 
 @AutoRouterConfig()
-class AppRouter extends _$AppRouter {
+class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(
           page: SplashRoute.page,
           path: '/',
+          initial: true,
+        ),
+        AutoRoute(
+          page: MainRoute.page,
+          path: '/main',
+          children: [
+            AutoRoute(
+              page: HomeRoute.page,
+              path: 'home',
+              initial: true,
+            ),
+            AutoRoute(
+              page: SettingsRoute.page,
+              path: 'settings',
+            ),
+          ],
         ),
       ];
 }
