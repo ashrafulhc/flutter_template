65049028c39b53310b49333589a3b9a6bc4a2075
Ashraful Haque Chowdhury <ashraful.rahi@gmail.com>
2024-09-02 01:41:07 +0600

    nit: removed unnecessary async

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_template/presentation/app/app_flavor.dart';
 import 'main_common.dart';
 
-void main() async {
+void main() {
   const config = AppFlavor.production;
   mainCommon(config);
 }
